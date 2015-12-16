10f8a856adf546935d031dfe9cf84a2543e0affd
Keith Raven <keith@keth.co.uk>
2015-12-16 23:48:16 +0000

    added async shapes module

---
+++ src/Orchard.Web/Modules/Raven.AsyncShapes/Controllers/TemplatesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Orchard;
+using Orchard.DisplayManagement.Descriptors;
+using Orchard.Mvc;
+using Raven.Api.DisplayManagement;
+
+namespace Raven.AsyncShapes.Controllers
+{
+    public class TemplatesController : Controller
+    {
+        private readonly IOrchardServices _services;
+
+        public TemplatesController(
+            IOrchardServices services) {
+            _services = services;
+        }
+
+        // GET: Templates
+        public ActionResult Index(string contentType, string displayType = "summary")
+        {
+            contentType = "Simple";
+            displayType = "Summary";
+
+            var model = _services.ContentManager.New(contentType);
+            var vm = _services.ContentManager.BuildDisplay(model);
+            return View(vm);
+        }
+    }
+}

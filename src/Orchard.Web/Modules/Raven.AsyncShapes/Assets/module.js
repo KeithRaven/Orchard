74a9d3ff360ff71989a76f1d0592a9604fd94c0b
Keith Raven <keith@keth.co.uk>
2015-12-18 00:16:10 +0000

    got basic content retrieval and display with angular

---
+++ src/Orchard.Web/Modules/Raven.AsyncShapes/Assets/module.js
@@ -0,0 +1,17 @@
+﻿angular.module("AsyncShapes", ["ngSanitize"]);
+
+angular.module("AsyncShapes").controller('AsyncShapesCntrl', ['$scope', '$http', '$sce', function ($scope, $http, $sce) {
+
+
+    $scope.updateItem = function (id) {
+
+        $http.get('/OrchardLocal/api/raven.api/Detail/' + id).then(function (response) {
+            $scope.model = response.data;
+        });
+
+    };
+
+
+
+    
+}]);
--- src/Orchard.Web/Modules/Raven.AsyncShapes/Controllers/ItemController.cs
+++ src/Orchard.Web/Modules/Raven.AsyncShapes/Controllers/ItemController.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Orchard.Themes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Raven.AsyncShapes.Controllers
-{
+{   [Themed]
     public class ItemController : Controller
     {
         // GET: Item
